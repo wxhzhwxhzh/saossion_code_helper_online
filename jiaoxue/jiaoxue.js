42584ae0f2f1a29c46fda363cf8a4bdecc3f37c3
wxhzhwxhzh <you@example.com>
2024-06-19 15:50:16 +0800

    美化实战代码模块

--- jiaoxue/jiaoxue.js
+++ jiaoxue/jiaoxue.js
@@ -266,6 +266,29 @@ while True:
 
 
 `
+window.human_mouse=`
+import autoit
+import time
+
+# 打开记事本
+autoit.run("notepad.exe")
+
+# 等待记事本窗口打开
+autoit.win_wait_active("[CLASS:Notepad]")
+
+# 获取当前时间
+current_time = time.strftime("%Y-%m-%d %H:%M:%S")
+print(current_time)
+
+# 输入当前时间到记事本
+autoit.send(current_time)
+
+# 等待一段时间，以便查看结果
+time.sleep(20)
+
+# 关闭记事本
+autoit.win_close("[CLASS:Notepad]")
+`
 
 
 function update_code(code){
@@ -305,6 +328,10 @@ document.getElementById("ua").addEventListener("change", function() {
         case "6":
             console.log("弹幕");
             update_code(window.jianting_danmu)
+            break;
+        case "7":
+            console.log("人类鼠标模拟");
+            update_code(window.human_mouse)
             break;
 
         default:
