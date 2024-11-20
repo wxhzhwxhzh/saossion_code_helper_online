12d9768fc5172ba0acd45b55f4c7c875c290294b
wxhzh <mygod@qq.com>
2024-11-20 20:06:49 +0800

    修复反斜杠错误

--- teach/code_cache.js
+++ teach/code_cache.js
@@ -714,7 +714,7 @@ from DrissionPage import ChromiumOptions
 # 创建浏览器启动配置对象
 options = ChromiumOptions()
 # 配置浏览器启动选项
-options.set_browser_path(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')  # 设置浏览器路径
+options.set_browser_path(r'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe')  # 设置浏览器路径
 options.ignore_certificate_errors() # 忽略证书错误
 options.no_imgs()  # 禁用图片
 options.headless(False)  # 无头模式
